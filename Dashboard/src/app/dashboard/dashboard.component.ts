250f96ad8253a515e9e5ef2e8b777b70b6bd03ff
snehabirodkar <snehabirodkar31@gmail.com>
2024-07-17 14:38:33 +0530

    cards created

--- Dashboard/src/app/dashboard/dashboard.component.ts
+++ Dashboard/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
